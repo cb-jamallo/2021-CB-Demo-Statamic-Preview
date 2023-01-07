446f19d09c7f67e6246132e7c44fdbe8eb6b5ae7
Joe Mallory <jamallo@castlebranch.com>
2023-01-07 04:49:44 -0500

    Update: Multiplte Adapters' added

--- storage/app/private/website-build-development/tekmountain-com/vite.config.js
+++ storage/app/private/website-build-development/tekmountain-com/vite.config.js
@@ -1,7 +1,15 @@
+// https://stackoverflow.com/questions/70709987/how-to-load-environment-variables-from-env-file-using-vite
+
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig, loadEnv } from 'vite';
 
-const config = {
-	plugins: [sveltekit()]
+/** @type {import('vite').UserConfig} */
+export default ({ mode }) => {
+    
+    // Extends 'process.env.*' with VITE_*-variables from '.env.(mode=production|development)'
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+    return defineConfig({
+        plugins: [sveltekit()]
+    }); 
 };
-
-export default config;
