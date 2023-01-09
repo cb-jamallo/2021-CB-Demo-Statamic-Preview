1e924a6a66384c7a1339a003dd154dfa1d2b817d
Joseph Mallory <jamallo@sysadmins-MacBook-Pro.local>
2023-01-09 04:45:50 -0500

    Automated Commit

--- app/Classes/SchemaClass.php
+++ app/Classes/SchemaClass.php
@@ -1573,7 +1573,7 @@ class SchemaClass
         $l = shell_exec( $buildDirectoryUserPath . ' git push -u origin main 2>&1' );
         $l = shell_exec( $buildDirectoryUserPath . ' git remote set-url origin git@github.com:cb-jamallo/2021-CB-Demo-Statamic-Preview.git 2>&1' );
         $l = shell_exec( $buildDirectoryUserPath . ' git push git@github.com:cb-jamallo/2021-CB-Demo-Statamic-Preview.git main 2>&1' );
-        $l = shell_exec( $buildDirectoryUserPath . ' git push https://cb-jamallo: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@github.com/cb-jamallo/2021-CB-Demo-Statamic-Preview.git main 2>&1');
+        $l = shell_exec( $buildDirectoryUserPath . ' git push https://cb-jamallo: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@github.com/cb-jamallo/2021-CB-Demo-Statamic-Preview.git main 2>&1');
         // $m = "Returned with status $return_var and output:\n";
         // $m = print_r($output, false );
         // SSH Teesting Command: ssh -T git@github.com
--- content/collections/website_controller/tekmountain-com.md
+++ content/collections/website_controller/tekmountain-com.md
@@ -82,7 +82,7 @@ development-code:
         	  %sveltekit.head%
         	</head>
         	<body>
-        	  	Yahtzee!!!!!!!!!!!!!!!
+        	  	Iron Maiden.
         		%sveltekit.body%
         	</body>
         </html>
@@ -173,5 +173,5 @@ local-code:
     type: item
     enabled: true
 updated_by: 3fcfe9a1-6362-444c-8d55-030541dd2f8d
-updated_at: 1673257159
+updated_at: 1673257268
 ---
