1c26ac7035fc5015b34b24a6e9bdcbd17decaab3
Joseph Mallory <jamallo@sysadmins-MacBook-Pro.local>
2023-01-08 20:55:38 -0500

    Automated Commit

--- app/Classes/SchemaClass.php
+++ app/Classes/SchemaClass.php
@@ -1556,8 +1556,9 @@ class SchemaClass
         //exec( 'git add ' . $buildEnv->path('') . ' https://jamallo:1976-Hanover-PA@github.com/cb-jamallo/2021-CB-Demo-Statamic-Preview.git 2>&1' );
         $l = exec( 'git add -A 2>&1' );
         $l = exec( 'git commit -m "Automated Commit" 2>&1' );
-        $l = exec( 'git push main https://cb-jamallo:1976-Hanover-PA@github.com/cb-jamallo/2021-CB-Demo-Statamic-Preview 2>&1' );
-    
+        $l = exec( 'git push git@github.com:cb-jamallo/2021-CB-Demo-Statamic-Preview.git main 2>&1' );
+
+        exec('cd ' . $buildEnv->path('') . $buildSlug . '&& git push origin main 2>&1' );
     }
 
     // Find and Replace string content for schema path shortcodes
