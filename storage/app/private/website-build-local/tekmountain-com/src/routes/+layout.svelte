f4083baafec08dd60d76c51d6e7b3261c4891e6e
Joseph Mallory <jamallo@CastleBranch-MacBook-Pro.local>
2023-01-28 15:52:41 -0500

    Automated Commit

--- storage/app/private/website-build-local/tekmountain-com/src/routes/+layout.svelte
+++ storage/app/private/website-build-local/tekmountain-com/src/routes/+layout.svelte
@@ -8,17 +8,7 @@
   //...
 
   /* Global Stores */
-  const websiteBuildJson = <script>
-
-  /* Svelte imports */
-  import { onMount, tick } from 'svelte';
-  import { page } from '$app/stores';
-  
-  /* Component imports */
-  //...
-
-  /* Global Stores */
-  const websiteBuildJson = [websiteBuildJson];
+  const websiteBuildJson = null;
   const websiteNavigation = <script>
 
   /* Svelte imports */
@@ -29,133 +19,7 @@
   //...
 
   /* Global Stores */
-  const websiteBuildJson = <script>
-
-  /* Svelte imports */
-  import { onMount, tick } from 'svelte';
-  import { page } from '$app/stores';
-  
-  /* Component imports */
-  //...
-
-  /* Global Stores */
-  const websiteBuildJson = [websiteBuildJson];
-  const websiteNavigation = [websiteBuild.navigation.json];
-  let websitePageClass = 'home';
-
-  onMount(async () => {
-	// #Await...
-	await tick();
-
-	console.log( $page );
-	console.log( websiteNavigation );
-
-  });
-</script>
-
-
-<svelte:head>
-</svelte:head>
-
-....
-<main id="main" class='main main-{ websitePageClass }'>
-  <pre>
-  	{@html JSON.stringify( { websiteBuildJson } ) }
-  </pre>
-  <slot />
-</main>;
-  const websiteNavigation = [websiteBuild.navigation.json];
-  let websitePageClass = 'home';
-
-  onMount(async () => {
-	// #Await...
-	await tick();
-
-	console.log( $page );
-	console.log( websiteNavigation );
-
-  });
-</script>
-
-
-<svelte:head>
-</svelte:head>
-
-....
-<main id="main" class='main main-{ websitePageClass }'>
-  <pre>
-  	{@html JSON.stringify( { websiteBuildJson } ) }
-  </pre>
-  <slot />
-</main>;
-  let websitePageClass = 'home';
-
-  onMount(async () => {
-	// #Await...
-	await tick();
-
-	console.log( $page );
-	console.log( websiteNavigation );
-
-  });
-</script>
-
-
-<svelte:head>
-</svelte:head>
-
-....
-<main id="main" class='main main-{ websitePageClass }'>
-  <pre>
-  	{@html JSON.stringify( { websiteBuildJson } ) }
-  </pre>
-  <slot />
-</main>;
-  const websiteNavigation = <script>
-
-  /* Svelte imports */
-  import { onMount, tick } from 'svelte';
-  import { page } from '$app/stores';
-  
-  /* Component imports */
-  //...
-
-  /* Global Stores */
-  const websiteBuildJson = <script>
-
-  /* Svelte imports */
-  import { onMount, tick } from 'svelte';
-  import { page } from '$app/stores';
-  
-  /* Component imports */
-  //...
-
-  /* Global Stores */
-  const websiteBuildJson = [websiteBuildJson];
-  const websiteNavigation = [websiteBuild.navigation.json];
-  let websitePageClass = 'home';
-
-  onMount(async () => {
-	// #Await...
-	await tick();
-
-	console.log( $page );
-	console.log( websiteNavigation );
-
-  });
-</script>
-
-
-<svelte:head>
-</svelte:head>
-
-....
-<main id="main" class='main main-{ websitePageClass }'>
-  <pre>
-  	{@html JSON.stringify( { websiteBuildJson } ) }
-  </pre>
-  <slot />
-</main>;
+  const websiteBuildJson = null;
   const websiteNavigation = [websiteBuild.navigation.json];
   let websitePageClass = 'home';
 
