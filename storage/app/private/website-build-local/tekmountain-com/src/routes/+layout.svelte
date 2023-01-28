aa87703b530a2fe38d4576811a600a1a67afddd0
Joseph Mallory <jamallo@CastleBranch-MacBook-Pro.local>
2023-01-28 16:13:05 -0500

    Automated Commit

--- storage/app/private/website-build-local/tekmountain-com/src/routes/+layout.svelte
+++ storage/app/private/website-build-local/tekmountain-com/src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script>
-
   /* Svelte imports */
   import { onMount, tick } from 'svelte';
   import { page } from '$app/stores';
@@ -8,13 +7,16 @@
   //...
 
   /* Global Stores */
+  /** @type {import('./$types').LayoutData} */
+  export let data;
+  
   let websitePageClass = 'home';
 
   onMount(async () => {
 	// #Await...
 	await tick();
 
-	console.log( page );
+	console.log( $page );
 
   });
 </script>
