f70ae94d72d6108c069699d054bb3afc64a13771
Joseph Mallory <jamallo@CastleBranch-MacBook-Pro.local>
2023-01-29 17:42:20 -0500

    Automated Commit

---
+++ storage/app/private/website-build-development/tekmountain-com/src/routes/+layout.js
@@ -0,0 +1,42 @@
+export const prerender = true;
+export const trailingSlash = 'always';
+export const ssr = false;
+
+/** @type {import('./$types').LayoutLoad} */
+export async function load({ fetch, params, url }) 
+{
+    let data = null;
+    let dataWebsiteReport = null;
+  	let dataWebsiteControllerReport = null;
+    let dataWebsiteBuildReport = null;
+  	let dataWebsiteBuildNavigation = null;
+  	
+  	if ( url.searchParams.get('websiteReport')  === 'true' )
+	{ 
+		data = await fetch( '/lib/data/website/websiteReport.js' );
+  		dataWebsiteReport = await data.json();
+	}
+  	
+    if ( url.searchParams?.get('websiteControllerReport') === 'true' )
+	{ 
+		data = await fetch( '/lib/data/website/websiteControllerReport.js' );
+  		dataWebsiteControllerReport = await data.json();
+	}
+  
+  	if ( url.searchParams?.get('websiteBuildReport') === 'true' )
+	{ 
+		data = await fetch( '/lib/data/website/websiteBuildReport.js' );
+  		dataWebsiteBuildReport = await data.json();
+	}
+  
+  	data = await fetch( '/lib/data/website/websiteBuildNavigation.js' );
+  	dataWebsiteBuildNavigation = await data.json();
+    
+  	return { 
+	  dataWebsiteReport,
+	  dataWebsiteControllerReport,
+	  dataWebsiteBuildReport,
+	  dataWebsiteBuildNavigation,
+	};
+  
+}
---
+++ storage/app/private/website-build-development/tekmountain-com/src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<script>
+  /* Svelte imports */
+  import { onMount, tick } from 'svelte';
+  import { page } from '$app/stores';
+  
+  /* Component imports */
+  //.....
+
+  /* Global Stores */
+  
+  /** @type {import('./$types').LayoutData} */
+  export let data;
+  
+  const pageRouteId = $page.route.id;
+  const pageName = ( pageRouteId === null ) 
+  	? 'error'
+  	:  ( pageRouteId === null )
+  		? 'home' 
+  		: pageRouteId.replace('/', '');
+  
+  onMount(async () => {
+	// #Await...
+	await tick();
+	
+	//$page.data = data
+  
+  });
+</script>
+
+
+<svelte:head>
+</svelte:head>
+
+<header class='header' tabindex='0'>
+</header>
+
+<main id="main" class='main main-{ pageName }'>
+  Hello World!!!
+  <slot />
+</main>
